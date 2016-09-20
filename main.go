b7a884a03d9967e528b360ffb5f20855c2b64ce1
datianshi <dsz0111@gmail.com>
2016-09-20 18:29:53 -0500

    update the dependencies

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+func main(){
+	//app := cli.NewApp()
+	//app.Name = "greet"
+	//app.Usage = "fight the loneliness!"
+	//app.Action = func(c *cli.Context) error {
+	//	fmt.Println("Hello friend!")
+	//	return nil
+	//}
+	//
+	//app.Run(os.Args)
+}
