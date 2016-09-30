be2e7c94fa7f96fa6cb387c984b50c88af4b01b1
datianshi <dsz0111@gmail.com>
2016-09-30 14:13:28 -0500

    Add test to build

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ if [ -z ${VERSION} ]; then
   echo "USAGE: VERSION NUMBER need to be provided"
   exit 1 
 fi
+go test $(glide novendor)
 GOOS=linux GOARCH=amd64 go build -o "out/linux/opsman-cli"
 GOOS=darwin GOARCH=amd64 go build -o "out/darwin/opsman-cli"
 GOOS=windows GOARCH=amd64 go build -o "out/windows/opsman-cli.exe"
